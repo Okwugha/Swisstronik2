8b6b33d241d95e844f338940fd4ab127e663f2cb
Akachukwu Anthony-Okwugha <akachukwuanthonyokwugha2004@gmail.com>
2024-08-05 16:01:09 +0000

    Initial commit

--- contracts/Token.sol
+++ contracts/Token.sol
@@ -4,7 +4,7 @@ pragma solidity ^0.8.9;
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
 contract TestToken is ERC20 {
-    constructor()ERC20("BGN","$BG"){} 
+    constructor()ERC20("BigMan","BGM"){} 
 
     function mint100tokens() public {
         _mint(msg.sender,100*10**18);
